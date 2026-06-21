73eea2d8610739c7c8291f2910840a7d66603e66
rana41-hub <pratapranak41@gmail.com>
2026-06-21 22:33:38 +0530

    Fragments and mapping learning

--- Basics/src/App.jsx
+++ Basics/src/App.jsx
@@ -1,14 +1,32 @@
 //Creating Component
 
-import FirstButton from "./button"
-import Dynamism from "./Dynamic";
+// import FirstButton from "./button"
+// import Dynamism from "./Dynamic";
+// function App() {
+
+//   return <div>
+//     <h1>First Component</h1>
+//     <FirstButton></FirstButton>
+//     <Dynamism></Dynamism>
+//   </div>
+// }
+
+// export default App;
+
+let goodElements = ["Protein" , "Iron" , "Creatine" , "D3" , "B12" , "Calcium"];
+
 function App() {
 
-  return <div>
-    <h1>First Component</h1>
-    <FirstButton></FirstButton>
-    <Dynamism></Dynamism>
-  </div>
+  return (
+  <>
+     <h1>Essential Body Elements</h1>
+     <ul className="list-group">
+       {goodElements.map((items) => (<li key={items} className="list-group-item">{items}</li>))};
+     </ul>
+  </>
+  );
 }
+
+//Providing unique key for each elements ensures rendering optimization.
 
 export default App;
---
+++ Basics/src/Notes/Fragments.jsx
@@ -0,0 +1,7 @@
+//Fragments allow grouping of multiple elements without adding extra Divs. It ensures cleaner DOM and consistent styling.
+
+//syntax --> import React from "react";
+          //<React.Fragment>.....</React.Fragment>
+
+
+//syntax2 --> No need to write import statement. Directly write <>....</>.      
